e10b9d9fc4d4f19e11088d3d51418dc9117c35c5
Adheesha Gamage <70915421+adheeshagamage@users.noreply.github.com>
2021-08-10 22:36:47 +0530

    Add files via upload

---
+++ Assignment3/q8/pyhash.py
@@ -0,0 +1,21 @@
+import sys
+import hashlib
+import string
+import secrets
+
+def getSaltAsString(length=32):
+    alpha = string.ascii_letters + string.digits
+    salt = ''.join(secrets.choice(alpha) for _ in range (length))
+    return salt
+
+def getHash(password,salt):
+    hashVal = hashlib.sha512((salt+password).encode())
+    return hashVal.hexdigest()
+
+if __name__ == '__main__':
+    password = str(sys.argv[1])
+    salt = getSaltAsString()
+    print("Password: {}\n".format(password))
+    print("salt = {}\n".format(salt))
+    hash = getHash(password,salt)
+    print(hash)                                              
